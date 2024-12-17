5d0ed72d36ba2c735a3f45bf1db65a1fd0aeeac2
VedNik3 <vedantnikam812@gmail.com>
2024-12-17 19:45:25 +0530

    api end points

--- frontend/Ionots/src/utils/constants.js
+++ frontend/Ionots/src/utils/constants.js
@@ -1,2 +1,4 @@
-export const API_END_POINT_User = "http://localhost:5000/api/user"
-export const API_END_POINT_Project = "http://localhost:5000/api/project"
+// export const API_END_POINT_User = "http://localhost:5000/api/user"
+// export const API_END_POINT_Project = "http://localhost:5000/api/project"
+export const API_END_POINT_User = "https://progress-report.onrender.com/api/user"
+export const API_END_POINT_Project = "https://progress-report.onrender.com/api/project"
